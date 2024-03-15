295338006c0fa4efe3921775ff8f26ba2d5474c1
Ehsaan <ec22881@qmul.ac.uk>
2024-03-15 19:16:00 +0000

    idk

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,24 @@
 // Header.js
 import './Header.css';
+import React, { useState } from 'react';
 function Header(){
+
+  const [searchInput, setSearchInput] = useState('');
+
+
+  const handleSearch = (event) => {
+    event.preventDefault(); // Prevents the default form submission behavior
+    // Do something with the searchInput value, for example, store it in a variable or call a function with it
+    console.log('Search query:', searchInput);
+    const city = searchInput
+    console.log(city)
+    
+  };
+
+  const handleChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
 return (
   <header className="header">
     <div className="header-left">
@@ -12,10 +30,18 @@ return (
       </div>
     </div>
     <div className="header-right">
-      {/* Search bar */}
-      <input className="search-bar"  type="text"  placeholder="   Search for Location..." />
-      <button>Search</button>
-    </div>
+        {/* Form with search bar */}
+        <form onSubmit={handleSearch}>
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="   Enter a city name"
+            value={searchInput}
+            onChange={handleChange}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
   </header>
 );
 };
